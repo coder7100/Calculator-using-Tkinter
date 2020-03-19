ac80fb80afa71d8ad8e195e97b765a404da62644
coder7100 <a.aundhkar7100@gmail.com>
2020-03-19 22:34:19 +0530

    adding entry

--- calculator.py
+++ calculator.py
@@ -5,5 +5,5 @@ window.geometry("400x400")
 
 # --entry field----
 entry1 = Entry(width=30, borderwidth=3)
-entry1.grid(column=50, row=0)
+entry1.grid(column=0, row=0, columnspan=3)
 window.mainloop()
